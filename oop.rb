93764e20bcd57c977b87f2837a1e8250a44ff1f1
Mostafa Sakr <msakr3@gmail.com>
2022-08-05 18:39:07 -0600

    Did exercises

--- oop.rb
+++ oop.rb
@@ -3,6 +3,26 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+
+  def initialize(name)
+
+    @name = name
+
+    @color = "silver"
+
+  end
+
+  def say
+    return "*~* #{@name} *~*"
+  end
+
+end
+
+unicorn1 = Unicorn.new("Charlie")
+
+print unicorn1.say
+
 
 
 #  Write a class called Vampire
@@ -11,6 +31,36 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+  attr_reader :name
+  def initialize(name, pet)
+
+    @name = name
+
+    if pet != nil
+      @pet = pet
+    else
+      @pet = "bat"
+    end
+
+    @thirsty = true
+
+  end
+
+  def drink
+    @thirsty = false
+  end
+
+  def identify
+    puts @name, @pet
+  end
+
+end
+
+vampire1 = Vampire.new("Dragula", "cat")
+
+vampire1.identify
+
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +68,44 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+
+  def initialize(name, rider, color)
+
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = true
+    @hunger = 0
+
+  end
+
+  def eats
+
+    @hunger = @hunger + 1
+
+    if @hunger < 4
+      @is_hungry = true
+    else
+      @is_hungry = false
+    end
+
+    puts @hunger
+    puts @is_hungry
+
+  end
+
+end
+
+hunger = 0
+dragon1 = Dragon.new("Sapphira", "Eragon", "Blue")
+
+dragon1.eats
+dragon1.eats
+dragon1.eats
+dragon1.eats
+
 
 
 #  Write a Hobbit class
@@ -28,3 +116,51 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+
+  def initialize(name, disposition)
+
+    @name = name
+
+    @disposition = disposition
+
+    @age = 0
+
+    @is_adult = false
+
+    @is_old = false
+
+    @has_ring = false
+
+    if @age >= 33
+      @is_adult = true
+    else
+      @is_adult = false
+    end
+
+    if @age >100
+      @is_old = true
+    else
+      @is_old = false
+    end
+
+    if name == "Frodo"
+      @has_ring = true
+    else
+      @has_ring = false
+    end
+
+  end
+
+  def celebrate_birthday
+      @age = @age + 1
+      puts @age, @is_adult, @is_old, @has_ring
+  end
+
+end
+
+hobbit1 = Hobbit.new("Sam", "Good")
+
+
+hobbit1.celebrate_birthday
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,14 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+**Answer: It was pretty fun. I had to double check syntax repeatedly though!**
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+**Answer: I got through a series of rarefactions and compressions! I think a have a problem of getting too hyper-focused and I need to force myself to take breaks.**
+
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+**Answer: next week? I'm guessing this should have been the 22nd. I have a nice 12 day break (from work as well) before I start mod 1, so I intend to charge my battery up and review what I've learned during mod 0 while possibly expanding upon it before mod 1 starts. If you could share any more exercises/challenges or resources that offer those (for our current level) to keep progressively challenging/building up our technical skills, that would be great!**
